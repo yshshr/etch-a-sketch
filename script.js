3467f54c3813d789d93bfa1844cfb3b57b0e7bf4
ls <65608254+yshshr@users.noreply.github.com>
2026-06-30 19:21:04 +0800

    添加重新生成画板按钮

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ for(let i = 0; i < 16 * 16; i++){
   container.appendChild(div);
 }
 
-const squares = document.querySelectorAll('.square-div');
-squares.forEach(squarediv => {
+function mouseOverOutHandler(squarediv){
   squarediv.addEventListener('mouseover', e=>{
     const target = e.target;
     target.classList.toggle('squarediv-over');
@@ -14,5 +13,27 @@ squares.forEach(squarediv => {
   squarediv.addEventListener('mouseout', e=>{
     const target = e.target;
     target.classList.toggle('squarediv-over');
+    target.style.backgroundColor = 'yellow';
   });
+}
+
+const squares = document.querySelectorAll('.square-div');
+squares.forEach(mouseOverOutHandler);
+
+const btn = document.querySelector('#recreate');
+btn.addEventListener('click', e=>{
+  const grids = Number(prompt('新画板每边的方格数，最大100',64));
+  if(grids>100) {
+    alert('最大方格数只能为100');
+    return;
+  }
+  container.replaceChildren();
+  for(let i=0; i<grids * grids; i++){
+    const div = document.createElement('div');
+    div.classList.add('square-div');
+    div.style.width = `${100 / grids}%`;
+    container.appendChild(div);
+  }
+  const squares = document.querySelectorAll('.square-div');
+  squares.forEach(mouseOverOutHandler);
 });
